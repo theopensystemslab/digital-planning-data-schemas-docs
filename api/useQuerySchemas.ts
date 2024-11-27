2c2f8661d60e662e4d6cc995323cf5592e4dcd63
Jo Humphrey <31373245+jamdelion@users.noreply.github.com>
2024-11-27 18:29:30 +0000

    Use hardcoded list of versions and query accordingly

--- api/useQuerySchemas.ts
+++ api/useQuerySchemas.ts
@@ -1,19 +1,27 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, UseQueryOptions } from "@tanstack/react-query";
 import axios from "axios";
 
 const DIGITAL_PLANNING_DATA_SCHEMAS_JSON_URL =
-  "https://theopensystemslab.github.io/digital-planning-data-schemas/v0.7.0/schema.json";
+  "https://theopensystemslab.github.io/digital-planning-data-schemas";
 
-const useQuerySchemas = () => {
+// TODO: type the version more strictly
+const useQuerySchemas = (
+  version: string,
+  options?: Readonly<Omit<UseQueryOptions<unknown>, "queryKey" | "queryFn">>
+) => {
   const fetchFn = async (url: string) => {
     const response = await axios.get(url);
     return response.data;
   };
 
   return useQuery({
-    queryKey: ["schemas"],
-    queryFn: () => fetchFn(DIGITAL_PLANNING_DATA_SCHEMAS_JSON_URL),
+    queryKey: ["schemas", version],
+    queryFn: () =>
+      fetchFn(
+        `${DIGITAL_PLANNING_DATA_SCHEMAS_JSON_URL}/${version}/schema.json`
+      ),
     staleTime: 1000 * 60 * 10, // cache for 10 minutes
+    ...options,
   });
 };
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Typography from "@mui/material/Typography";
+import { useQueryClient } from "@tanstack/react-query";
 
 import { JsonSchemaViewer } from "@stoplight/json-schema-viewer";
 import { injectStyles } from "@stoplight/mosaic";
@@ -8,18 +9,30 @@ import useQuerySchemas from "./../api/useQuerySchemas";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import Select from "@mui/material/Select";
+import { useState } from "react";
 import Header from "./components/Header";
+
+// TODO: grab this programatically instead of hard-coding
+const LATEST_WORKING_VERSION = "v0.7.0";
 
 const App = () => {
   injectStyles();
+  const queryClient = useQueryClient();
+  const [selectedVersion, setSelectedVersion] = useState(
+    LATEST_WORKING_VERSION
+  );
 
-  const { data: schema, isLoading, isError } = useQuerySchemas();
+  const {
+    data: schema,
+    isLoading,
+    isError,
+  } = useQuerySchemas(selectedVersion, { retry: false });
 
   const formik = useFormik({
     initialValues: {
-      version: "",
+      version: selectedVersion,
     },
-    onSubmit: () => console.log("hi"),
+    onSubmit: (values) => setSelectedVersion(values.version),
   });
 
   return (
@@ -29,21 +42,51 @@ const App = () => {
       ) : (
         <>
           <form onSubmit={formik.handleSubmit}>
-            <InputLabel>Version</InputLabel>
-            <Select>
-              <MenuItem>A version</MenuItem>
+            <InputLabel shrink id="select-version-label">
+              Version
+            </InputLabel>
+            <Select
+              sx={{ marginBottom: 4 }}
+              labelId="select-version-label"
+              defaultValue={selectedVersion}
+              id="select-version"
+              onChange={(e) => {
+                queryClient.invalidateQueries({
+                  queryKey: ["schemas", selectedVersion],
+                });
+                formik.setFieldValue("version", e.target.value, false);
+                formik.submitForm();
+              }}
+              value={formik.values.version}
+            >
+              // TODO: add query to populate with a list of possible versions
+              <MenuItem value={"v0.7.1"}>v0.7.1</MenuItem>
+              <MenuItem value={"v0.7.0"}>v0.7.0</MenuItem>
+              <MenuItem value={"v0.6.0"}>v0.6.0</MenuItem>
+              <MenuItem value={"v0.5.0"}>v0.5.0</MenuItem>
+              <MenuItem value={"v0.4.0"}>v0.4.0</MenuItem>
+              <MenuItem value={"v0.4.1"}>v0.4.1</MenuItem>
+              <MenuItem value={"v0.3.0"}>v0.3.0</MenuItem>
+              <MenuItem value={"v0.2.3"}>v0.2.3</MenuItem>
+              <MenuItem value={"v0.2.2"}>v0.2.2</MenuItem>
+              <MenuItem value={"v0.2.1"}>v0.2.1</MenuItem>
+              <MenuItem value={"v0.2.0"}>v0.2.0</MenuItem>
+              <MenuItem value={"v0.1.2"}>v0.1.2</MenuItem>
+              <MenuItem value={"v0.1.1"}>v0.1.1</MenuItem>
+              <MenuItem value={"v0.1.0"}>v0.1.0</MenuItem>
             </Select>
           </form>
-          <Typography pb={2}>{schema.$id}</Typography>
-          <JsonSchemaViewer
-            name="Digital planning data schemas"
-            schema={schema}
-            hideTopBar={false}
-            emptyText="No schema defined"
-            expanded={true}
-            defaultExpandedDepth={0}
-            renderRootTreeLines={true}
-          />
+          {!isError && (
+            <JsonSchemaViewer
+              name="Digital planning data schemas"
+              schema={schema}
+              hideTopBar={false}
+              emptyText="No schema defined"
+              expanded={true}
+              defaultExpandedDepth={0}
+              renderRootTreeLines={true}
+            />
+          )}
         </>
       )}
       {isError && (
