0f9b255bdfaaf390d4df83e6a9215bcf4b835271
Jessica McInchak <jessica.mcinchak@gmail.com>
2025-05-05 12:19:46 +0200

    update deps and final copy

--- src/components/Enums.tsx
+++ src/components/Enums.tsx
@@ -9,6 +9,9 @@ const Enums = ({ schema }: any) => {
       <Typography variant="h5" mt={3} mb={1}>
         Enums
       </Typography>
+      <Typography variant="body2" mb={2}>
+        Enums visualise the hierarchical nature of select "any of" values referenced throughout the specification above. The <code>parent.child</code> structure is informed by Plan✕ service design which presents users a single entry point (eg apply for planning permission) and then smartly funnels into discrete journeys based on granular property type, project types, applicable constraints, and more.
+      </Typography>
       <TreeDiagram schema={schema} />
     </Box>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import { Provider as MosaicProvider, injectStyles } from "@stoplight/mosaic";
 import { ComponentProps } from "react";
@@ -15,6 +16,10 @@ const Header = ({ children }: ComponentProps<"header">) => {
         Digital Planning Data schemas aim to encourage more interoperability and
         consistency between systems by offering a central, version controlled
         specification for documenting and validating planning data.
+      </Typography>
+      <Typography pt={2} maxWidth={1000} mx="auto">
+        These schemas are currently developed by and exchanged between <Link href="https://opendigitalplanning.org/" target="_blank">Open Digital Planning</Link> products including Plan✕,
+        Back Office Planning System (BOPS), and Digital Planning Register (DPR). Learn more, share feedback, and report issues on <Link href="https://github.com/theopensystemslab/digital-planning-data-schemas" target="_blank">GitHub</Link>.
       </Typography>
       <MosaicProvider>
         <Box
